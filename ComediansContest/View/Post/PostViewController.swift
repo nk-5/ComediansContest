c3b47ce94c49afe77957f994c954fe2a9a4a10ce
nkws <keigo_nakagawa@voyagegroup.com>
2018-01-03 23:44:22 +0900

    get photo library's data

--- ComediansContest/View/Post/PostViewController.swift
+++ ComediansContest/View/Post/PostViewController.swift
@@ -11,6 +11,10 @@ class PostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // TODO:
+        // photo library's data show in collection view
+        // PHAuthorization status check
+        
         let momentLists: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .momentList, subtype: .any, options: nil)
         let folderLists: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .folder, subtype: .any, options: nil)
         let smartFolderList: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .smartFolder, subtype: .any, options: nil)
@@ -18,5 +22,26 @@ class PostViewController: UIViewController {
         print(momentLists)
         print(folderLists)
         print(smartFolderList)
+
+        momentLists.enumerateObjects({ momentCollectionList, _, _ in
+            print(momentCollectionList)
+
+            let moments: PHFetchResult = PHAssetCollection.fetchMoments(inMomentList: momentCollectionList, options: nil)
+
+            moments.enumerateObjects({ momentAssetCollection, _, _ in
+                let assets: PHFetchResult = PHAsset.fetchAssets(in: momentAssetCollection, options: nil)
+                print(assets)
+
+                assets.enumerateObjects({ asset, _, _ in
+
+                    PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil, resultHandler: { result, info in
+                        print(result)
+                        print(info)
+                        let imageView: UIImageView = UIImageView(image: result)
+                        self.view.addSubview(imageView)
+                    })
+                })
+            })
+        })
     }
 }
