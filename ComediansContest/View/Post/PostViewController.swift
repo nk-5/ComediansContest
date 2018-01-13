5af754778836248c5d0e356fd2be59e3baf50e8d
nkws <keigo_nakagawa@voyagegroup.com>
2018-01-13 21:49:50 +0900

    delete photo library images

--- ComediansContest/View/Post/PostViewController.swift
+++ ComediansContest/View/Post/PostViewController.swift
@@ -25,36 +25,6 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
         // https://dev.classmethod.jp/references/ios8-photo-kit-7/
         // Camera sample code
         // https://developer.apple.com/library/content/samplecode/PhotoPicker/Listings/PhotoPicker_APLViewController_m.html
-
-        // get images from photo library
-        //        let momentLists: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .momentList, subtype: .any, options: nil)
-        //        let folderLists: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .folder, subtype: .any, options: nil)
-        //        let smartFolderList: PHFetchResult = PHCollectionList.fetchCollectionLists(with: .smartFolder, subtype: .any, options: nil)
-        //
-        //        print(momentLists)
-        //        print(folderLists)
-        //        print(smartFolderList)
-        //
-        //        momentLists.enumerateObjects({ momentCollectionList, _, _ in
-        //            print(momentCollectionList)
-        //
-        //            let moments: PHFetchResult = PHAssetCollection.fetchMoments(inMomentList: momentCollectionList, options: nil)
-        //
-        //            moments.enumerateObjects({ momentAssetCollection, _, _ in
-        //                let assets: PHFetchResult = PHAsset.fetchAssets(in: momentAssetCollection, options: nil)
-        //                print(assets)
-        //
-        //                assets.enumerateObjects({ asset, _, _ in
-        //
-        //                    PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil, resultHandler: { result, info in
-        //                        print(result)
-        //                        print(info)
-        //                        let imageView: UIImageView = UIImageView(image: result)
-        //                        self.view.addSubview(imageView)
-        //                    })
-        //                })
-        //            })
-        //        })
     }
 
     @IBAction func didTouchLibrary(_: Any) {
